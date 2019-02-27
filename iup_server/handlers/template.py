bdc473a80797009ba787ebc3884b65d367cc739c
InvisibleUp <invisibleup@outlook.com>
2019-02-27 00:38:45 -0500

    Changes to template.py to load from content directory.
    FYI, all page content changes are in a private branch.

--- iup_server/handlers/template.py
+++ iup_server/handlers/template.py
@@ -1,22 +1,25 @@
 import functools
-from flask import render_template
+from flask import render_template_string
 from ..sitepage import SitePage
 
 
 def TemplateHandler(sitepage: SitePage):
     page = sitepage.page
     if not 'template' in page['dir']:
-        template = page['dir'] + '/index.html'
+        template_path = page['dir'] + '/index.html'
     else:
-        template = page['template']
+        template_path = page['template']
     bc = sitepage.get_breadcrumb(sitepage.pagesdict, sitepage.dictpath)
     pd = sitepage.get_path_depth()
 
+    with sitepage.app.open_resource('content' + template_path, mode='r') as f:
+        template = f.read()
+
     sitepage.app.add_url_rule(
-        rule=page['dir'],
-        endpoint='/'.join(bc),
+        rule=page['dir'] + '/',
+        endpoint='/'.join(bc) + '/',
         view_func=functools.partial(
-            render_template,
+            render_template_string,
             template,
             breadcrumb=bc,
             pathdepth=pd,
