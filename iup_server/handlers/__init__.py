6dc94473fac693c078bb76f7b3b742264ffb799e
InvisibleUp <invisibleup@outlook.com>
2019-02-27 00:49:29 -0500

    Added Markdown template handler.
    I don't really like the code duplication...

--- iup_server/handlers/__init__.py
+++ iup_server/handlers/__init__.py
@@ -2,8 +2,9 @@ from ..sitepage import SitePage
 from ..constants import PAGEATTRIBS
 
 from .template import TemplateHandler
+from .markdown import MarkdownHandler
 
 HANDLERS = {
     'TemplateHandler': TemplateHandler,
-    'MarkdownHandler': TemplateHandler,
+    'MarkdownHandler': MarkdownHandler,
 }
--- iup_server/handlers/markdown.py
+++ iup_server/handlers/markdown.py
@@ -1,15 +1,16 @@
 import functools
 import logging
 
+import markdown
 from flask import render_template_string
 
 from ..sitepage import SitePage
 
 
-def TemplateHandler(sitepage: SitePage):
+def MarkdownHandler(sitepage: SitePage):
     page = sitepage.page
     if not 'template' in page['dir']:
-        template_path = page['dir'] + '/index.html'
+        template_path = page['dir'] + '/index.md'
     else:
         template_path = page['template']
     bc = sitepage.get_breadcrumb(sitepage.pagesdict, sitepage.dictpath)
@@ -23,6 +24,8 @@ def TemplateHandler(sitepage: SitePage):
     except FileNotFoundError:
         logging.exception('Content file not found')
         return
+
+    template = markdown.markdown(template)
 
     sitepage.app.add_url_rule(
         rule=page['dir'] + '/',
